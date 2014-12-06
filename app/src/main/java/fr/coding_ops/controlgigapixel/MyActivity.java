8270cc53a97f3a35363909610e6c86766b174c09
Alix <ducrosalix@hotmail.fr>
2014-12-07 00:04:05 +0100

    Discovery of bletooth devices

--- app/src/main/java/fr/coding_ops/controlgigapixel/MyActivity.java
+++ app/src/main/java/fr/coding_ops/controlgigapixel/MyActivity.java
@@ -3,10 +3,14 @@ package fr.coding_ops.controlgigapixel;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -21,7 +25,9 @@ import java.util.Set;
 
 public class MyActivity extends Activity {
 
+    private List<String> mArrayAdapter = new ArrayList<String>();
     private static final int REQUEST_ENABLE_BT = 0x0001;
+    private BluetoothAdapter mBluetoothAdapter = null ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +35,8 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
 
         //Here we check if the device has bluetooth capability
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
         if (mBluetoothAdapter == null) {
             Toast.makeText(this, "Bluetooth non supporté", Toast.LENGTH_SHORT).show();
         }
@@ -43,13 +50,31 @@ public class MyActivity extends Activity {
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
             }
 
-            //Here we fill the listView with paired bluetooth devices
-            List<BluetoothDevice> pairedDevices = new ArrayList<BluetoothDevice>(mBluetoothAdapter.getBondedDevices());
-            //ListView listView = (ListView)findViewById(R.id.listBluetoothDevices);
-            //ArrayAdapter<BluetoothDevice> adapter = new ArrayAdapter<BluetoothDevice>(this.getApplicationContext(),
-            //        android.R.layout.simple_list_item_1, pairedDevices);
-            //listView.setAdapter(adapter);
+            BroadcastReceiver mReceiver = new BroadcastReceiver() {
+                public void onReceive(Context context, Intent intent) {
+                    String action = intent.getAction();
+                    // When discovery finds a device
+                    if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                        // Get the BluetoothDevice object from the Intent
+                        BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                        // Add the name and address to an array adapter to show in a ListView
+                        Toast.makeText(getApplication(), device.getName() + "\n" + device.getAddress(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            };
 
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mReceiver, filter);
+
+//            mBluetoothAdapter.startDiscovery();
+
+            //Here we fill the listView with paired bluetooth devices
+            ListView listView = (ListView)findViewById(R.id.listBluetoothDevices);
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getApplicationContext(),
+                    android.R.layout.simple_list_item_1, mArrayAdapter);
+            listView.setAdapter(adapter);
+
+/*
             JoystickView joystickView = (JoystickView)findViewById(R.id.joystickView) ;
             final TextView joystickState = (TextView)findViewById(R.id.joystickState);
 
@@ -58,11 +83,18 @@ public class MyActivity extends Activity {
                 @Override
                 public void onValueChanged(int i, int i2, int i3) {
 
-                    joystickState.setText("Zob : "+String.valueOf(i)+" "+String.valueOf(i2)+" "+String.valueOf(i3));
+                    joystickState.setText("Angle : "+String.valueOf(i)+"\nMotion : "+String.valueOf(i2)+"\nDirection :  "+String.valueOf(i3));
                 }
             }, JoystickView.DEFAULT_LOOP_INTERVAL);
+*/
 
+        }
+    }
 
+    public void startDiscover(View view)
+    {
+        if(mBluetoothAdapter != null) {
+            mBluetoothAdapter.startDiscovery();
         }
     }
 
